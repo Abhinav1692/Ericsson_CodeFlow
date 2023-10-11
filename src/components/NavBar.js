fad7b86a972696e0752df981803b8235f8f6bdf7
Aniket Tiwari <aniket.76907@gmail.com>
2023-10-11 20:30:23 +0530

    Updated NavBar and landing page UI

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,46 +1,39 @@
-import React from 'react'; 
-import { 
-  Nav, 
-  NavLink, 
-  Bars, 
-  NavMenu, 
-  NavBtn, 
-  NavBtnLink, 
-} from './NavbarElements'; 
-  
-const NavBar = () => { 
-  return ( 
-    <> 
-      <Nav> 
-        <Bars /> 
-  
-        <NavMenu> 
-          <NavLink to='/about' activeStyle> 
-            About 
-          </NavLink> 
-          <NavLink to='/events' activeStyle> 
-            Events 
-          </NavLink> 
-          <NavLink to='/annual' activeStyle> 
-            Annual Report 
-          </NavLink> 
-          <NavLink to='/team' activeStyle> 
-            Teams 
-          </NavLink> 
-          <NavLink to='/blogs' activeStyle> 
-            Blogs 
-          </NavLink> 
-          <NavLink to='/sign-up' activeStyle> 
-            Sign Up 
-          </NavLink> 
-          {/* Second Nav */} 
-          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */} 
-        </NavMenu> 
-        <NavBtn> 
-          <NavBtnLink to='/signin'>Sign In</NavBtnLink> 
-        </NavBtn> 
-      </Nav> 
-    </> 
-  ); 
-}; 
+import React from 'react';
+import "./style/NavBar.css"
+
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <img src="ericsson_codeFlow.png" alt="Stack Overflow Logo" width="50px" height="50px"/>
+      </div>
+      <ul className="navbar-menu">
+        <li className="navbar-item">
+          <a href="/questions">Questions</a>
+        </li>
+        <li className="navbar-item">
+          <a href="/tags">Tags</a>
+        </li>
+        <li className="navbar-item">
+          <a href="/trending">Trending</a>
+        </li>
+        <li className="navbar-item">
+          <a href="/about">About</a>
+        </li>
+      </ul>
+      <div className="navbar-search">
+        <input type="text" placeholder="Search..." />
+      </div>
+      <div className="logIn">
+        <button className="logIn">Login</button>
+      </div>
+      
+      <div className="signUp">
+        <button className="signUp">signUp</button>
+      </div>
+    </div>
+  );
+}
+
 export default NavBar;
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Content from "../components/Content";
+import Trending from "../components/Trending";
+import SideBar from "../components/SideBar";
+import "./Home.css";
 
-
-function Home(){
-    return(
-        <NavBar/>
+function Home() {
+    return (
+        <div className="home-container">
+            <NavBar />
+            <div className="sidebar"> {/* Use class name "sidebar" */}
+                <SideBar />
+            </div>
+            <div className="content"> {/* Use class name "content" */}
+                <Content />
+            </div>
+            <div className="trending"> {/* Use class name "trending" */}
+                <Trending />
+            </div>
+        </div>
     );
 }
 
